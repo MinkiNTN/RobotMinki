e326f9a6231b7087d237afaa0d7f5cce4b3c6fd4
MinkiNTN <hugo6644493@gmail.com>
2022-03-29 21:38:33 +0300

    [Nisekoi-Lookup] Switch to pymediawiki

--- nisekoi_lookup.py
+++ nisekoi_lookup.py
@@ -2,17 +2,14 @@ import discord
 from discord.ext import commands
 from config_helper import ConfigHelper
 import shared
-import fandom
+from mediawiki import MediaWiki
 
 class NisekoiLookup(commands.Cog, name='Nisekoi lookup tool', description='Fetching Nisekoi related stuff from Fandom Wiki'):
     config = ConfigHelper.load_config()
+    wiki = MediaWiki('https://nisekoi.fandom.com/api.php')
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        fandom.set_wiki("Nisekoi")
     
     nisekoi_lookup = discord.SlashCommandGroup('nisekoi-lookup', 'Fetching Nisekoi related stuff from Fandom Wiki', guild_ids=config['guild_ids'])
 
@@ -20,7 +17,7 @@ class NisekoiLookup(commands.Cog, name='Nisekoi lookup tool', description='Fetch
     async def character(self, 
             ctx: discord.ApplicationContext,
             name: discord.Option(str, "Choose your favorite Nisekoi character.", required = True, autocomplete=shared.get_nisekoi_character)):
-        page = fandom.page(name)
+        page = self.wiki.page(name)
         await ctx.respond('[Testing] Page summary: {}'.format(page.summary))
 
 def setup(bot):
